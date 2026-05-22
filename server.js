56bdb31f387b873f08e3499e0a2cb4d8e934a1af
Otton Hernandez Jara <otton04@gmail.com>
2026-05-22 01:05:25 -0600

    fix: update server.js (assistant)

--- server.js
+++ server.js
@@ -160,6 +160,7 @@ app.post("/identify", async (req, res) => {
         family: details.taxonomy?.family ?? "",
         genus: details.taxonomy?.genus ?? "",
       },
+description: details.description?.value ?? "",
       watering: details.watering ?? details.best_watering ?? "",
       careLevel: details.care_level ?? "",
       type: details.type ?? "",
