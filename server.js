a855c2d4d35b3491973785ef29d4c11974ce5e3b
Otton Hernandez Jara <otton04@gmail.com>
2026-05-22 18:19:04 -0600

    chore: commit from assistant

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.post("/identify", async (req, res) => {
         family: details.taxonomy?.family ?? "",
         genus: details.taxonomy?.genus ?? "",
       },
-description: details.description?.value || `RAW:${JSON.stringify(details.description)}`,
+description: details.best_watering ?? details.description?.value ?? "",
       watering: details.watering ?? details.best_watering ?? "",
       careLevel: details.care_level ?? "",
       type: details.type ?? "",
